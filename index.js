4dcbae505be7dd18cf7b7bc80f6fbe09954d98a8
Kai Davenport <kaiyadavenport@gmail.com>
2014-08-06 14:48:59 -0700

    move to hyperprox

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-var locked = require('locked')
-var hyperquest = require('hyperquest')
+var hyperprox = require('hyperprox')
 
 module.exports = function(selectedfn, addressfn){
-
+	var proxy = hyperprox(addressfn)
+	var proxyhandler = proxy.handler()
 	return function(handler){
 		return function(req, res){
 			if(req.method=='GET'){
@@ -15,28 +15,7 @@ module.exports = function(selectedfn, addressfn){
 					return handler(req, res)
 				}
 				else{
-					var address = addressfn()
-						
-					if(!address){
-						res.statusCode = 500
-						res.end('no master address found')
-						return
-					}
-					
-					var proxy = hyperquest('http://' + address + req.url, {
-						method:req.method,
-						headers:req.headers
-					})
-					if(req.method=='GET'||req.method=='DELETE'){
-						proxy.pipe(res)
-					}
-					else{
-						req.pipe(proxy).pipe(res)
-					}
-					proxy.on('error', function(err){
-						res.statusCode = 500
-						res.end(err)
-					})
+					proxyhandler(req, res)
 				}
 			}
 		}
